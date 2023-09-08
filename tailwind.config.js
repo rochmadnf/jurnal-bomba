860e9369fdb74f9cb844fad70ee3157cb6f3a79f
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-09-09 02:41:20 +0800

    set font family and button style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+                bungee: ["Bungee", ...defaultTheme.fontFamily.serif],
+            },
+        },
     },
     plugins: [require("flowbite/plugin")],
 };
