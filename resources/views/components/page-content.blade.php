d0414b6d94ad81828c60119cf406ec461f8d8107
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-09-12 20:07:53 +0800

    update style and anchor target

--- resources/views/components/page-content.blade.php
+++ resources/views/components/page-content.blade.php
@@ -2,7 +2,7 @@
     <h1 class="text-4xl font-bold dark:text-white">{{ $subTitle }}</h1>
     <div class="mb-8 mt-4 border-b-[1px] border-b-slate-300/20"></div>
     <article
-        class="prose prose-slate dark:prose-invert prose-p:text-lg prose-p:leading-10 prose-p:tracking-wider prose-a:no-underline dark:prose-p:text-white dark:prose-a:text-amber-500">
+        class="prose prose-slate dark:prose-invert prose-p:text-lg prose-p:leading-10 prose-p:tracking-wider prose-a:no-underline dark:prose-p:text-white dark:prose-a:text-amber-500 dark:hover:prose-a:text-amber-600">
         {{ $slot }}
     </article>
 </section>
--- resources/views/pages/policy/check-plagiarism.blade.php
+++ resources/views/pages/policy/check-plagiarism.blade.php
@@ -2,7 +2,7 @@
     <x-page-content subTitle="Cek Plagiarisme">
         <p>
             Naskah yang diajukan ke <strong>Jurnal Bomba</strong> akan dilakukan pengecekan plagiarisme menggunakan alat
-            pendeteksi plagiarisme <a href="https://turnitin.com">Turnitin</a> dan <a
+            pendeteksi plagiarisme <a target="_blank" href="https://turnitin.com">Turnitin</a> dan <a target="_blank"
                 href="https://plagiarismcheckerx.com">Plagiarism Checker X</a>.
         </p>
 
