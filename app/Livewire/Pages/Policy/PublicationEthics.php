5c2f885f5695561b096584587c0e2e903c0628ed
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-09-14 01:26:23 +0800

    publication ethics page

---
+++ app/Livewire/Pages/Policy/PublicationEthics.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\Pages\Policy;
+
+use Artesaos\SEOTools\Facades\SEOTools;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title('Etika Publikasi')]
+class PublicationEthics extends Component
+{
+    public function mount(){
+        SEOTools::metatags()->setDescription('Halaman Informasi terkait Etika Publikasi pada Bomba: Jurnal Pembangunan Daerah')
+    }
+    public function render()
+    {
+        return view('pages.policy.publication-ethics');
+    }
+}
---
+++ resources/views/pages/policy/publication-ethics.blade.php
@@ -0,0 +1,156 @@
+<div class="space-y-6">
+    <x-page-content subTitle="Etika Publikasi">
+        <p class="text-justify">
+            Pernyataan kode etik publikasi ilmiah merupakan pernyataan kode etik untuk semua pihak
+            yang terlibat dalam proses publikasi jurnal ilmiah, diantaranya: Pengelola, Editor, Mitra
+            Bestari, dan Penulis/Author. Pernyataan kode etika publikasi ilmiah ini mengacu pada
+            Peraturan Kepala LIPI Nomor 5 Tahun 2014 tentang Kode Etika Publikasi Ilmiah, yang pada
+            intinya menjunjung tiga nilai etik dalam publikasi, yaitu:
+        </p>
+
+        <ol class="text-justify md:-ml-1 md:text-lg">
+            <li>Kenetralan, yakni bebas dari pertentangan kepentingan dalam pengelolaan publikasi;</li>
+            <li>Keadilan, yakni memberikan hak kepengarangan kepada yang berhak sebagai
+                pengarang/penulis; dan</li>
+            <li>
+                Kejujuran, yakni bebas dari Duplikasi, Fabrikasi, Falsifikasi, dan Plagiarisme (DF2P)
+                dalam publikasi.
+            </li>
+        </ol>
+
+        <h3 class="pe-sub">Tugas dan Tanggung Jawab Pemimpin Jurnal</h3>
+        <ol class="text-justify md:ml-2 md:text-lg">
+            <li>
+                Menentukan nama jurnal, lingkup keilmuan, keberkalaan, dan pengajuan akreditasi apabila
+                diperlukan. Menentukan keanggotaan dewan editor.
+            </li>
+            <li>
+                Mendefinisikan hubungan antara penerbit, editor, mitra bestari, dan pihak lain dalam suatu kontrak.
+            </li>
+            <li>
+                Menghargai hal-hal yang bersifat rahasia, baik untuk peneliti yang berkontribusi, pengarang/penulis,
+                editor, maupun mitra bestari.
+            </li>
+            <li>
+                Menerapkan norma dan ketentuan mengenai hak atas kekayaan intelektual, khususnya hak cipta.
+            </li>
+            <li>
+                Melakukan telaah kebijakan jurnal dan menyampaikannya kepada pengarang/penulis, dewan editor,
+                mitra bestari, dan pembaca.
+            </li>
+            <li>
+                Membuat panduan aturan dan etika perilaku bagi editor dan mitra bestari.
+            </li>
+            <li>
+                Mempublikasikan jurnal secara teratur.
+            </li>
+            <li>
+                Menjamin ketersediaan sumber dana untuk keberlanjutan penerbitan jurnal.
+            </li>
+            <li>
+                Membangun jaringan kerja sama lembaga penelitian dan instansi terkait
+            </li>
+            <li>
+                Mempersiapkan perizinan dan aspek legalitas lainnya.
+            </li>
+        </ol>
+
+        {{-- *Editor --}}
+        <h3 class="pe-sub">Tugas dan Tanggung Jawab Editor</h3>
+        <ol class="text-justify md:ml-2 md:text-lg">
+            <li>
+                Mempertemukan kebutuhan pembaca dan penulis, serta mengupayakan peningkatan mutu publikasi secara
+                berkelanjutan.
+            </li>
+            <li>
+                Menerapkan proses untuk menjamin mutu karya tulis yang dipublikasikan.
+            </li>
+            <li>
+                Mengedepankan kebebasan berpendapat secara objektif.
+            </li>
+            <li>
+                Memelihara integritas rekam jejak akademik penulis.
+            </li>
+            <li>
+                Menyampaikan koreksi, klarifikasi, dan permintaan maaf apabila diperlukan.
+            </li>
+            <li>
+                Bertanggung jawab terhadap gaya dan format penulisan karya tulis ilmiah, sedangkan isi dan segala
+                pernyataan dalam karya tulis merupakan tanggung jawab penulis.
+            </li>
+            <li>
+                Secara aktif meminta pendapat penulis, mitra bestari, dan anggota dewan editor untuk meningkatkan mutu
+                publikasi.
+            </li>
+            <li>
+                Melakukan evaluasi internal secara berkala terhadap kualitas jurnal ilmiah.
+            </li>
+            <li>
+                Mendorong penulis untuk melakukan perbaikan karya tulis hingga layak terbit.
+            </li>
+        </ol>
+
+        {{-- *Reviewer --}}
+        <h3 class="pe-sub">
+            Tugas dan
+            Tanggung Jawab Mitra
+            Bestari (<i>Reviewer</i>)</h3>
+        <ol class="text-justify md:ml-2 md:text-lg">
+            <li>
+                Menelaah karya tulis dan menyampaikan hasil telaahan kepada editor, sebagai bahan penentuan
+                kelayakan suatu karya tulis untuk diterbitkan.
+            </li>
+            <li>
+                Penelaah tidak melakukan telaah ilmiah atas karya tulis yang melibatkan dirinya, baik secara langsung
+                maupun tidak langsung.
+            </li>
+            <li>
+                Memberikan saran, masukan dan rekomendasi positif terhadap manuskrip karya tulis ilmiah.
+            </li>
+            <li>
+                Menjaga privasi penulis dengan tidak menyebarluaskan hasil koreksi, saran, dan rekomendasi
+                terhadap suatu manuskrip.
+            </li>
+            <li>
+                Mendorong penulis melakukan perbaikan karya tulis.
+            </li>
+            <li>
+                Menelaah kembali karya tulis yang telah diperbaiki sesuai dengan standar yang telah ditentukan.
+            </li>
+            <li>
+                Karya tulis ditelaah tepat waktu sesuai gaya selingkung terbitan berdasarkan kaidah ilmiah (metode
+                pengumpulan data, legalitas pengarang, kesimpulan, dan lain-lain) yang telah ditetapkan
+            </li>
+        </ol>
+
+        <h3 class="pe-sub">Tugas dan Tanggung Jawab Penulis</h3>
+        <ol class="text-justify md:ml-2 md:text-lg">
+            <li>
+                Memastikan bahwa yang termasuk dalam daftar penulis telah memenuhi kriteria sebagai penulis.
+            </li>
+            <li>
+                Bertanggung jawab secara kolektif untuk pekerjaan dan isi artikel meliputi metode, analisis,
+                perhitungan, dan rinciannya.
+            </li>
+            <li>
+                Menyatakan asal sumber daya (termasuk pendanaan), baik secara langsung maupun tidak langsung.
+            </li>
+            <li>
+                Menjelaskan keterbatasan dalam penelitian
+            </li>
+            <li>
+                Menanggapi komentar yang dibuat oleh mitra bestari dan dewan redaksi secara profesional ilmiah
+                dan tepat waktu.
+            </li>
+            <li>
+                Menginformasikan secara tertulis kepada editor jika akan menarik kembali karya tulisnya.
+            </li>
+            <li>
+                Membuat pernyataan bahwa karya tulis yang diserahkan untuk diterbitkan adalah asli, dan belum
+                pernah dipublikasikan di manapun dan dalam bahasa apapun, dan tidak sedang dalam proses
+                pengajuan ke penerbit lain.
+            </li>
+        </ol>
+
+    </x-page-content>
+</div>
