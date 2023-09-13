56d2a1c582b47f9508296327e6af10b27dab1759
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-09-14 01:36:56 +0800

    add method to top

--- resources/js/scroll-position.js
+++ resources/js/scroll-position.js
@@ -3,20 +3,34 @@ window.addEventListener("scroll", () => {
 });
 
 window.addEventListener("livewire:navigated", () => {
+    toWindowTop();
     initNavbar();
 });
 
 window.addEventListener("load", () => {
+    toWindowTop();
     initNavbar();
 });
 
+function toWindowTop() {
+    window.scrollTo({ behavior: "smooth", top: 0 });
+}
+
 function initNavbar() {
     const navBar = document.querySelector(".navbar");
+    const btnToTop = document.getElementById("btnToTop");
+
+    btnToTop.addEventListener("click", () => {
+        toWindowTop();
+    });
+
     if (window.pageYOffset > 0) {
         navBar.previousElementSibling.classList.remove("hidden");
         navBar.style.position = "fixed";
+        btnToTop.style.display = "flex";
     } else {
         navBar.previousElementSibling.classList.add("hidden");
         navBar.style.position = "sticky";
+        btnToTop.style.display = "none";
     }
 }
