c094b9019265417065db3d174f0953e1d91d5992
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-09-14 01:29:16 +0800

    refactor app layouts
    - add button to top
    - load favicon from components
    - add texture background when open in xlarge device

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,58 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     {!! \Artesaos\SEOTools\Facades\SEOTools::generate() !!}
-
-    {{-- favicon --}}
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('favicon/apple-touch-icon-57x57.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('favicon/apple-touch-icon-114x114.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('favicon/apple-touch-icon-72x72.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144"
-        href="{{ asset('favicon/apple-touch-icon-144x144.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('favicon/apple-touch-icon-60x60.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120"
-        href="{{ asset('favicon/apple-touch-icon-120x120.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('favicon/apple-touch-icon-76x76.png') }}" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152"
-        href="{{ asset('favicon/apple-touch-icon-152x152.png') }}" />
-    <link rel="icon" type="image/png" href="{{ asset('favicon/196x196.png') }}" sizes="196x196" />
-    <link rel="icon" type="image/png" href="{{ asset('favicon/96x96.png') }}" sizes="96x96" />
-    <link rel="icon" type="image/png" href="{{ asset('favicon/32x32.png') }}" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{ asset('favicon/16x16.png') }}" sizes="16x16" />
-    <link rel="icon" type="image/png" href="{{ asset('favicon/128.png') }}" sizes="128x128" />
-    <meta name="application-name" content="{{ env('APP_NAME') }}" />
-    <meta name="msapplication-TileColor" content="#FFFFFF" />
-    <meta name="msapplication-TileImage" content="{{ asset('favicon/mstile-144x144.png') }}" />
-    <meta name="msapplication-square70x70logo" content="{{ asset('favicon/mstile-70x70.png') }}" />
-    <meta name="msapplication-square150x150logo" content="{{ asset('favicon/mstile-150x150.png') }}" />
-    <meta name="msapplication-wide310x150logo" content="{{ asset('favicon/mstile-310x150.png') }}" />
-    <meta name="msapplication-square310x310logo" content="{{ asset('favicon/mstile-310x310.png') }}" />
-    {{-- end favicon --}}
+    <x-constants.favicon />
 
     <title>{{ $title }} &mdash; {{ config('app.name') }}</title>
-
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="max-w-[1440px] bg-accent-1 text-gray-900 antialiased dark:bg-slate-900 dark:text-gray-300">
-    <header class="bg-accent-1 dark:bg-gray-800">
+<body class="body-app">
+    <div id="body-texture"></div>
+    <header class="bg-accent-1 dark:bg-slate-900">
         <div id="shadow-navbar" class="hidden w-full bg-transparent md:h-[90px]"></div>
         <x-navbar />
     </header>
 
     @if (in_array(url()->current(), [route('home'), route('login'), route('register')]))
-        <main class="px-6">
+        <main class="mb-12 px-6 dark:bg-slate-900">
             {{ $slot }}
         </main>
     @else
-        <main class="mt-12 grid place-content-center gap-10 md:grid-cols-9 md:px-48">
-            <div class="md:col-span-6">
+        <main class="grid px-24 py-12 dark:bg-slate-900 md:grid-cols-9 md:place-content-center md:gap-10">
+            <div class="col-span-7 h-fit rounded-md md:col-span-6">
                 {{ $slot }}
             </div>
             <x-navside />
-
         </main>
     @endif
+
+    <div id="btnToTop"
+        class="fixed bottom-10 right-5 flex items-center rounded-full p-2 dark:bg-amber-600 dark:text-white dark:hover:bg-amber-700"
+        role="button" aria-label="to top">
+        @svg('icon-arrow-up', 'h-5 w-5')
+    </div>
 </body>
 
 </html>
